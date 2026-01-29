323a05788dfd909ea4a0172e4e31cbfbcd123198
SrinivasaRaoKamani-Infosys <srinivasarao.k31@infosys.com>
2026-01-30 01:18:29 +0530

    Update build.js

--- frontend/build.js
+++ frontend/build.js
@@ -1,1 +1,31 @@
+const fs = require("fs");
 
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>React Frontend</title>
+</head>
+<body>
+<h1>React Frontend</h1>
+<button onclick="callApi()">Call Backend</button>
+<p id="result"></p>
+
+<script>
+function callApi() {
+fetch('/api/message')
+.then(res => res.json())
+.then(data => {
+document.getElementById('result').innerText = data.message;
+})
+.catch(err => {
+document.getElementById('result').innerText = 'Error calling backend';
+});
+}
+</script>
+</body>
+</html>
+`;
+
+fs.mkdirSync("build", { recursive: true });
+fs.writeFileSync("build/index.html", html);
