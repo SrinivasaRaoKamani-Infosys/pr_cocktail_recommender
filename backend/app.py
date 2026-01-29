4532dbe3ce7ee39f6136603668837a1bc57f802f
SrinivasaRaoKamani-Infosys <srinivasarao.k31@infosys.com>
2026-01-30 01:21:25 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,15 @@
+from flask import Flask, jsonify
+import os
 
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+return jsonify(status="Backend is healthy")
+
+@app.route("/api/message")
+def message():
+return jsonify(message="Hello from Python Backend!")
+
+if __name__ == "__main__":
+app.run(host="0.0.0.0", port=5000)
