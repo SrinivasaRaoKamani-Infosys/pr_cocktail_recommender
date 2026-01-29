1a7562ed66cd706977a3ed7da3f2c6cd1aacee7b
SrinivasaRaoKamani-Infosys <srinivasarao.k31@infosys.com>
2026-01-30 00:47:17 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,19 @@
+variable "location" {
+type = string
+}
 
+variable "resource_group_name" {
+type = string
+}
+
+variable "acr_login_server" {
+type = string
+}
+
+variable "frontend_image" {
+default = "frontend-app:latest"
+}
+
+variable "backend_image" {
+default = "backend-app:latest"
+}
